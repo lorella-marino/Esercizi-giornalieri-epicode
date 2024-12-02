3a2666ebc3d697ce10c98cd78faceac8bb5205c9
“lorella.marino” <lorellamarino@gmail.com>
2024-12-02 17:03:05 +0100

    caricamento 02.12

--- 02.12_Pratica S2-L1/D1.js
+++ 02.12_Pratica S2-L1/D1.js
@@ -69,4 +69,4 @@ console.log(name1 !== name2)
 
 let name1 = "john"
 let name2 = "John" 
-console.log(name1.toLowerCase == name2.toLowerCase)
+console.log(name1.toLowerCase() == name2.toLowerCase())
